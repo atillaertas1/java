f9b9ecb34e446fea06974579880c64ea26653cf2
Atilla <109938381+atillaertas1@users.noreply.github.com>
2023-01-21 14:24:55 +0300

    3

---
+++ arraylist.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class main{
+
+  public static void main(String args[]){
+    ArrayList<String>  list = new ArrayList<String>();
+    //ArrayList<String> list2 = new ArrayList<Integer>();  example
+    
+    // add elements
+    list.add("Ankit");
+    list.add("Charu");
+    list.add("Bharti");
+    list.add("Sushant");
+    
+   System.out.println(list.get(2));
+  }
+
+}
