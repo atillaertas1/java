6ff652b2245a12a64beb55fefc12211fc21ac18f
Atilla <109938381+atillaertas1@users.noreply.github.com>
2023-01-21 14:32:11 +0300

    4

---
+++ hashset.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+   
+    HashSet<String> set=new HashSet();  
+    set.add("Bir");
+    set.add("İki");
+    set.add("Üç"); 
+    set.add("Dört");
+    set.add("Beş");
+ 
+  }
+}
